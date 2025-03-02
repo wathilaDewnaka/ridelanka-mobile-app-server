839a0f1e1e38d6d31ffbbb083b379e36bf90c573
Thareen-Sajana <thareensajana0@gmail.com>
2025-03-02 13:12:02 +0530

    Added train-test split functionality

--- ml/predictor.py
+++ ml/predictor.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 start = datetime.now()
@@ -13,3 +14,5 @@ y = dataset.iloc[:, -1].values
 
 print("X shape:", X)
 print("y shape:", y)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
