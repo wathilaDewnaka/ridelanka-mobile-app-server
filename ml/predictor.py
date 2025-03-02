189e0d2e7120d1683586258c4855f62846bde01a
Thareen-Sajana <thareensajana0@gmail.com>
2025-03-02 14:31:15 +0530

    Added OneHotEncoding for categorical features

--- ml/predictor.py
+++ ml/predictor.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 
 start = datetime.now()
 
@@ -16,3 +17,11 @@ print("X shape:", X)
 print("y shape:", y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('cat', OneHotEncoder(handle_unknown='ignore'), X.columns[:4])  # Encode first 4 columns
+    ],
+    remainder='passthrough'  # Keep numerical column (Total Distance)
+)
