ec1c3af1c44a7f495c030a11cd76ee9327058f92
Thareen-Sajana <thareensajana0@gmail.com>
2025-03-02 13:07:24 +0530

    import dataset

--- ml/predictor.py
+++ ml/predictor.py
@@ -1,2 +1,15 @@
-# Index.py
-# API Logic
+from datetime import datetime
+import pandas as pd
+
+
+start = datetime.now()
+
+# Load dataset
+dataset = pd.read_csv("../dataset/dataset_school_van.csv")
+
+
+X = dataset.iloc[:, :-1]
+y = dataset.iloc[:, -1].values
+
+print("X shape:", X)
+print("y shape:", y)
