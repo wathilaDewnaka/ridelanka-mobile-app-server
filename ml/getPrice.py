bf02679620ecc70f84370e23c7e6c49384d7d5be
Thareen-Sajana <thareensajana0@gmail.com>
2025-03-02 21:45:56 +0530

    Implemented price prediction function

--- ml/getPrice.py
+++ ml/getPrice.py
@@ -1,4 +1,10 @@
 import joblib
+import pandas as pd
 
 model = joblib.load("trained_model.pkl")
 print("Model loaded successfully!")
+
+def predict_price(from_city, to_city, vehicle_type, service_type, distance):
+    new_data = pd.DataFrame([[from_city, to_city, vehicle_type, service_type, distance]],
+                            columns=['From City', 'To City', 'Vehicle Type', 'Service Type', 'Total Distance (km)'])
+    return model.predict(new_data)[0]
